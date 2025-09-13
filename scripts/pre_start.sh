257151986e20a06c34b5f16d934b0b15cc8ea02c
Amos Setio <am05mhz@live.com>
2025-09-13 10:18:53 +0700

    fix install app reqs

--- scripts/pre_start.sh
+++ scripts/pre_start.sh
@@ -48,15 +48,6 @@ setup_comfy() {
 }
 
 if [ -d "/workspace" ]; then
-    if [ -d "/setup/app" ]; then
-        cp -r /setup/app /workspace/app
-        source /workspace/miniconda3/bin/activate comfy
-        cd /workspace/app
-        pip install --no-cache-dir -r requirements.txt
-    fi
-    if [ -f "/setup/custom_nodes.txt" ]; then
-        cp /setup/custom_nodes.txt /workspace/custom_nodes.txt
-    fi
     if [ ! -d "/workspace/miniconda3" ]; then
         echo "*** installing miniconda ***"
         setup_miniconda
